c8e96ca1b440755d9f2319fd789376373d8d3e0b
eugenev3 <eugenev3@gmail.com>
2015-01-23 06:05:03 +0000

    frontpage layout updates

--- scripts/fb.js
+++ scripts/fb.js
@@ -1,9 +1,30 @@
-var myApp = angular.module("myApp", ["firebase"]);
+  var myApp = angular.module("myApp", ["firebase"]);
 
-myApp.controller('MyController', ['$scope', '$firebase',
-  function($scope, $firebase) {
-    var ref = new Firebase("https://n9hhz66gqk8.firebaseio-demo.com/");
-    $scope.messages = $firebase(ref).$asArray();
-    //Code here
-  }
-]);
+  myApp.controller('MyController', ['$scope', '$firebase',
+    function($scope, $firebase) {
+      //CREATE A FIREBASE REFERENCE
+      var ref = new Firebase("https://n9hhz66gqk8.firebaseio-demo.com/");
+
+      // GET MESSAGES AS AN ARRAY
+      $scope.messages = $firebase(ref).$asArray();
+      
+      //ADD MESSAGE METHOD
+      $scope.addMessage = function(e) {
+
+        //LISTEN FOR RETURN KEY
+        if (e.keyCode === 13 && $scope.msg) {
+          //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+          var name = $scope.name || 'anonymous';
+
+          //ADD TO FIREBASE
+          $scope.messages.$add({
+            from: name,
+            body: $scope.msg
+          });
+
+          //RESET MESSAGE
+          $scope.msg = "";
+        }
+      }
+    }
+  ]);
