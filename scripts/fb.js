76de602caea9732fc8345848d10511a5358c42da
eugenev3 <eugenev3@gmail.com>
2015-01-28 03:17:46 +0000

    firebase integration data push and fetch

--- scripts/fb.js
+++ scripts/fb.js
@@ -3,7 +3,7 @@
   myApp.controller('MyController', ['$scope', '$firebase',
     function($scope, $firebase) {
       //CREATE A FIREBASE REFERENCE
-      var ref = new Firebase("https://n9hhz66gqk8.firebaseio-demo.com/");
+      var ref = new Firebase("https://aestheticdrift.firebaseio.com/web/saving-data/fireblog/posts");
 
       // GET MESSAGES AS AN ARRAY
       $scope.messages = $firebase(ref).$asArray();
@@ -26,6 +26,9 @@
           $scope.msg = "";
         }
       }
+      
+      console.log($scope.messages);
+      
     }
   ]);
   
@@ -40,32 +43,80 @@
       templateUrl: 'views/search.html'
     });
 });
+
+myApp.controller('modalController', function($scope) {
+    var showForm;
   
-  myApp.controller('modalController', function($scope) {
-      var showForm;
-    
-      $scope.isClicked = function() {
-        //debugger;
-        
-        if (showForm == !false) {
-          $scope.showForm = false;
-          console.log("cklicked", showForm);
-        } else {
-          $scope.showForm = true;
-        }
-          console.log("cklicked", showForm);
+    $scope.isClicked = function() {
+      //debugger;
       
+      if (showForm == !false) {
+        $scope.showForm = false;
+        console.log("cklicked", showForm);
+      } else {
+        $scope.showForm = true;
       }
+        console.log("cklicked", showForm);
     
-      
+    }
+  
+    
+});
+
+myApp.controller('myForm', ['$scope', '$firebase',
+function($scope, $firebase) {
+
+$scope.master = {};
+
+var ref = new Firebase("https://aestheticdrift.firebaseio.com/web/saving-data/fireblog");
+// set() with unique id
+
+/*var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});*/
+
+
+
+$scope.submitForm = function(user) {
+  console.log("user", user);
+  
+  var postsRef = ref.child("posts");
+  
+  postsRef.push({
+    author: user.name,
+    title: user.msg
   });
   
-/*myApp.directive('pacoForm', function() {
-  return {
-    restrict: 'AE',
-    templateUrl: 'views/form.html',
-    link: function($scope, element, attrs) {
-      console.log($scope);
-    }
-  };
-});*/
+  $scope.reset();
+  // $scope.master = angular.copy(user);
+};
+      
+$scope.reset = function(user) {
+  //alert("my form");
+  $scope.user = '';
+}
+//UPDATE OBJCT
+// var hopperRef = usersRef.child("gracehop");
+// hopperRef.update({
+//   "nickname": "eugene"
+// });
+
+  // GET MESSAGES AS AN ARRAY
+  // $scope.messages = $firebase(ref).$asArray();
+      
+  // $scope.submitForm = function (data) {
+  //         $scope.messages.set({
+  //           from: $scope.username,
+  //           body: $scope.msg
+  //         });
+  // };
+  
+}]);
