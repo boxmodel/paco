0cac4ea2f54bdc599b389152a80a90857765ec55
eugenev3 <eugenev3@gmail.com>
2015-03-01 05:16:07 +0000

    default page now has video mockup view

--- scripts/fb.js
+++ scripts/fb.js
@@ -1,53 +1,67 @@
-  var myApp = angular.module("myApp", ["firebase", "ngRoute", "ngAnimate"]);
-  var ref = new Firebase("https://aestheticdrift.firebaseio.com/web/saving-data/fireblog/posts");
+var myApp = angular.module("myApp", ["firebase", "ngRoute", "ngAnimate"]);
+var ref = new Firebase("https://aestheticdrift.firebaseio.com/web/saving-data/fireblog/posts");
+var updateItem = {};
 
-  myApp.controller('MyController', ['$scope', '$firebase',
-    function($scope, $firebase) {
-      //CREATE A FIREBASE REFERENCE
+myApp.controller('MyController', ['$scope', '$firebase',
+  function($scope, $firebase) {
+    //CREATE A FIREBASE REFERENCE
 
-      // GET MESSAGES AS AN ARRAY
-      $scope.messages = $firebase(ref).$asArray();
-      
-      //ADD MESSAGE METHOD
-      $scope.addMessage = function(e) {
+    // GET MESSAGES AS AN ARRAY
+    $scope.messages = $firebase(ref).$asArray();
 
-        //LISTEN FOR RETURN KEY
-        if (e.keyCode === 13 && $scope.msg) {
-          //ALLOW CUSTOM OR ANONYMOUS USER NAMES
-          var name = $scope.name || 'anonymous';
+    //ADD MESSAGE METHOD
+    $scope.addMessage = function(e) {
 
-          //ADD TO FIREBASE
-          $scope.messages.$add({
-            from: name,
-            body: $scope.msg
-          });
+      //LISTEN FOR RETURN KEY
+      if (e.keyCode === 13 && $scope.msg) {
+        //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+        var name = $scope.name || 'anonymous';
 
-          //RESET MESSAGE
-          $scope.msg = "";
-        }
+        //ADD TO FIREBASE
+        $scope.messages.$add({
+          from: name,
+          body: $scope.msg
+        });
+
+        //RESET MESSAGE
+        $scope.msg = "";
       }
+    };
     
-      $scope.findscope = function() {
-        var x = this;
-        //alert("my form");
-        //$scope.user = '';
-        console.log(x.item);
-      };
-      
-      $scope.languages = ['english', 'spanish', 'indonesia'];
+    $scope.languages = ['english', 'spanish', 'indonesia'];
+    
+    $scope.openModal = function(e) {
+      $scope.isOpen = true;
+    };
+    
+    $scope.closeModal = function(e) {
+      $scope.isOpen = !true;
+    };
 
-    }
-  ]);
+  }
+]);
   
-  myApp.config(function($locationProvider, $routeProvider) {
+myApp.config(function($locationProvider, $routeProvider) {
     $routeProvider
     .when('/home', {
-      templateUrl: 'views/search.html', 
+      templateUrl: 'views/home.html', 
       controller: ''
     })
     .when('/form', {
       templateUrl: 'views/form.html', 
       controller: ''
+    })
+    .when('/english/', {
+      templateUrl: 'views/english.html', 
+      controller: 'englishController'
+    })
+    .when('/spanish/', {
+      templateUrl: 'views/spanish.html', 
+      controller: 'spanishController'
+    })
+    .when('/indonesia/', {
+      templateUrl: 'views/indonesia.html', 
+      controller: 'indonesiaController'
     })
     .when('/english/:itemTitle', {
       templateUrl: 'views/english.html', 
@@ -67,42 +81,6 @@
     });
 });
 
-myApp.controller('modalController', function($scope) {
-    var showForm;
-  
-    $scope.isClicked = function() {
-      //debugger;
-      
-      if (showForm == !false) {
-        $scope.showForm = false;
-        console.log("cklicked", showForm);
-      } else {
-        $scope.showForm = true;
-      }
-        console.log("cklicked", showForm);
-    
-    }
-  
-    
-});
-
-// language controllers
-myApp.controller('englishController', function($scope, $routeParams) {
-  $scope.titleMessage = "English";
-  $scope.item = $routeParams.itemTitle;
-});
-
-myApp.controller('indonesiaController', function($scope, $routeParams) {
-  $scope.titleMessage = "Indonesia";
-  $scope.item = $routeParams.itemTitle;
-});
-
-myApp.controller('spanishController', function($scope, $routeParams) {
-  $scope.titleMessage = "Spanish";
-  $scope.item = $routeParams.itemTitle;
-});
-
-
 myApp.controller('navController',
   function($scope) {
     
@@ -120,70 +98,75 @@ myApp.controller('navController',
 
     };
     
-    $scope.languages = ['english', 'spanish', 'INDONESIA'];
+    $scope.languages = ['english', 'spanish', 'indonesia'];
     
-  });
-
-
-
-
+});
 
 myApp.controller('myForm', ['$scope', '$firebase',
 function($scope, $firebase) {
 
-  $scope.master = {};
-
   var ref = new Firebase("https://aestheticdrift.firebaseio.com/web/saving-data/fireblog");
-// set() with unique id
-
-/*var usersRef = ref.child("users");
-usersRef.set({
-  alanisawesome: {
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing"
-  },
-  gracehop: {
-    date_of_birth: "December 9, 1906",
-    full_name: "Grace Hopper"
-  }
-});*/
-
-
+  var d = new Date();
+  
+  $scope.today = Date.parse(d);
 
   $scope.submitForm = function(user) {
-    //console.log("user", user);
-    
     var postsRef = ref.child("posts");
-    
     postsRef.push({
       author: user.name,
-      title: user.msg,
+      title: user.title,
       language: user.language
     });
-    
     $scope.reset();
-    // $scope.master = angular.copy(user);
   };
-      
+
   $scope.reset = function(user) {
-    //alert("my form");
     $scope.user = '';
   };
 
-//UPDATE OBJCT
-// var hopperRef = usersRef.child("gracehop");
-// hopperRef.update({
-//   "nickname": "eugene"
-// });
-
-  // GET MESSAGES AS AN ARRAY
-  // $scope.messages = $firebase(ref).$asArray();
-      
-  // $scope.submitForm = function (data) {
-  //         $scope.messages.set({
-  //           from: $scope.username,
-  //           body: $scope.msg
-  //         });
-  // };
-  
 }]);
+
+/*repeat UGC form fields*/
+myApp.directive('ngFormElements', function() {
+  return {
+    transclude: true,
+    template: '<div class=\"group\">' +
+              '   <div class=\"flex-1\"><label>Language:</label></div>' +
+              '   <div class=\"flex-1\">' +
+              '       <div ng-repeat=\"item in messages\"><input for=\"user.language.\" ng-model=\"user.language\" type=\"checkbox\"/> {{item.language}}  </div>' +
+              '   </div>' +
+              '</div>',
+    replace: true,
+    restrict: 'E'
+  }
+});
+
+
+//ng-modal isOpen
+myApp.directive('ngModal', function() {
+  return {
+    restrict: 'A',
+    template: '<div class="modal-mask flexbox-column">' +
+                '<div class="modal display-flex margin-auto">' +
+                  '<div ng-click="closeModal()" class="close cursor">X</div>' +
+                  '<div class="margin-auto">hello world</div>' +
+                '</div>' +
+              '</div>'
+  }
+});
+
+// language controllers
+myApp.controller('englishController', function($scope, $routeParams) {
+    $scope.titleMessage = "English";
+    $scope.item = $routeParams.itemTitle;
+});
+
+myApp.controller('indonesiaController', function($scope, $routeParams) {
+  $scope.titleMessage = "Indonesia";
+  $scope.item = $routeParams.itemTitle;
+});
+
+myApp.controller('spanishController', function($scope, $routeParams) {
+  $scope.titleMessage = "Spanish";
+  $scope.item = $routeParams.itemTitle;
+});
